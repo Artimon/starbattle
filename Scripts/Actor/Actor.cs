815978cec779eff3bf9b9d409a42c5ecf8febb54
Artimon <webmaster@pad-soft.de>
2024-10-31 22:01:57 +0100

    Adds basic raycast implementation for action selection (WIP).

--- Scripts/Actor/Actor.cs
+++ Scripts/Actor/Actor.cs
@@ -11,12 +11,15 @@ public partial class Actor : NetworkNode3D {
 	public Node3D cameraTarget;
 
 	[Export]
+	public RayCast3D _rayCast;
+
+	[Export]
 	public StateMachine stateMachine;
 
 	[Export]
 	public StateMove stateMove;
 
-	public float inputX;
+	// public float inputX;
 
 	public float angle;
 
@@ -44,10 +47,10 @@ public partial class Actor : NetworkNode3D {
 		// GlobalPosition += velocity;
 	}
 
-	[Rpc(MultiplayerApi.RpcMode.AnyPeer)]
-	public void _SetInput(float value) {
-		inputX = value;
-	}
+	// [Rpc(MultiplayerApi.RpcMode.AnyPeer)]
+	// public void _SetInput(float value) {
+	// 	inputX = value;
+	// }
 
 	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
 	public void _MoveTo(Vector3 position) {
@@ -78,37 +81,88 @@ public partial class Actor : NetworkNode3D {
 	public bool _TryGetClickPosition(Vector2 mousePosition, out Vector3 clickPosition) {
 		var camera = GetViewport().GetCamera3D();
 		var from = camera.ProjectRayOrigin(mousePosition);
-		var to = from + camera.ProjectRayNormal(mousePosition) * 1000f;
+		var to = camera.ProjectRayNormal(mousePosition) * 1000f;
 
-		var plane = new Plane(Vector3.Up, 0);
-		var intersection = plane.IntersectsRay(from, to);
+		_rayCast.GlobalPosition = from;
+		_rayCast.TargetPosition = to;
+		_rayCast.ForceRaycastUpdate();
 
-		if (intersection == null) {
+		if (!_rayCast.IsColliding()) {
 			clickPosition = Vector3.Zero;
 
 			return false;
 		}
 
-		clickPosition = (Vector3)intersection;
+		clickPosition = _rayCast.GetCollisionPoint();
+		clickPosition.Y = 0f;
+
+		var collider = _rayCast.GetCollider();
+		if (collider is not ActorBody3D actorBody) {
+			return true;
+		}
+
+		GD.Print($"Detected collision with {actorBody.Actor.Name}");
 
 		return true;
 
+		// var camera = GetViewport().GetCamera3D();
+		// var from = camera.ProjectRayOrigin(mousePosition);
+		// var to = from + camera.ProjectRayNormal(mousePosition) * 1000f;
+		//
+		// var plane = new Plane(Vector3.Up, 0);
+		// var intersection = plane.IntersectsRay(from, to);
+		//
+		// if (intersection == null) {
+		// 	clickPosition = Vector3.Zero;
+		//
+		// 	return false;
+		// }
+		//
+		// clickPosition = (Vector3)intersection;
+		//
+		// return true;
 	}
+
+	public enum ActionTypes {
+		Move,
+		Attack
+	}
+
+	public ActionTypes _actionType;
 
 	public override void _Input(InputEvent @event) {
 		if (!IsPlayer) {
 			return;
 		}
 
+		if (@event.IsActionPressed("Move")) {
+			_actionType = ActionTypes.Move;
+		}
+
+		if (@event.IsActionPressed("Attack")) {
+			_actionType = ActionTypes.Attack;
+		}
+
 		if (@event is InputEventMouseButton { Pressed: true } mouseEvent) {
 			var success = _TryGetClickPosition(mouseEvent.Position, out var clickPosition);
-			if (success) {
+			if (!success) {
+				GD.Print($"Failed to get click position");
+				return;
+			}
+
+			GD.Print($"Clicked at {clickPosition} with {_actionType}");
+
+			if (_actionType == ActionTypes.Move) {
 				Rpc(nameof(_MoveTo), clickPosition);
+			}
+
+			if (_actionType == ActionTypes.Attack) {
+				// Attack
 			}
 		}
 
-		inputX = Input.GetAxis("Left", "Right");
-
-		Rpc(nameof(_SetInput), inputX);
+		// inputX = Input.GetAxis("Left", "Right");
+		//
+		// Rpc(nameof(_SetInput), inputX);
 	}
 }
---
+++ Scripts/Actor/ActorBody3D.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+
+namespace Starbattle;
+
+[GlobalClass]
+public partial class ActorBody3D : CharacterBody3D {
+	[Export]
+	public Actor _actor;
+
+	public Actor Actor => _actor;
+
+	// public override void _Input(InputEvent @event) {
+	// 	if (@event is InputEventMouseButton { Pressed: true } mouseEvent) {
+	// 		GD.Print($"Mouse pressed on {_actor.Name}");
+	// 	}
+	// }
+}
